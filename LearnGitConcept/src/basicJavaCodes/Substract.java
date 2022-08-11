20b6b91910425448ccabc476f31736b1339942d4
PriyanshMaheshwari-1234 <priyanshmaheshwari95@gmail.com>
2022-08-11 14:44:56 +0530

    Third modification it is

--- LearnGitConcept/src/basicJavaCodes/Substract.java
+++ LearnGitConcept/src/basicJavaCodes/Substract.java
@@ -10,10 +10,10 @@ public class Substract {
 		System.out.println(c);
 		
 		
-		int a =10;
-		int b =20;
-		int c = a+b;
-		System.out.println(c);
+		int d =10;
+		int e =20;
+		int f = d+e;
+		System.out.println(f);
 	}
 
 }
